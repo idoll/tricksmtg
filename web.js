3d056484e5c39b01260ac8189ab4122e1b6cd399
Ian Doll <idoll@in4772.local>
2013-06-23 02:10:49 -0400

    added Mongoose

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://idoll:temppw@dharma.mongohq.com:10054/app16467904');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Welcome to the Magic Card Webservice');
 });
 
 var port = process.env.PORT || 5000;
